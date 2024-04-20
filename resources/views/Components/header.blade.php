7a5847ecaa82a05284a51c6e9b122d5d8d076e66
Rheyan <rjvblanco.chmsu@gmail.com>
2024-04-20 10:26:07 +0800

    SEO Meta Updates

--- resources/views/Components/header.blade.php
+++ resources/views/Components/header.blade.php
@@ -3,6 +3,24 @@
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta charset="utf-8">
+<meta property="og:title" content="Core Support Hub">
+<meta property="og:description" content="Core Support Hub: Your Partner of choice for Small and Midsize Enterprises">
+<meta property="og:image" content="https://www.coresupporthub.com/images/CSH.png">
+<meta property="og:url" content="https://www.coresupporthub.com">
+<meta property="og:type" content="website">
+
+<meta name="description" content="Your Partner of choice for Small and Midsize Enterprises">
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Core Support Hub",
+  "url": "https://www.coresupporthub.com",
+  "logo": "https://www.coresupporthub.com/images/CSH.png",
+  "description": "Your Partner of choice for Small and Midsize Enterprises"
+}
+</script>
 <link rel="icon" href="{{ asset('images/cs_icon2.png') }}" type="image/x-icon">
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Poppins:400,500,600%7CTeko:300,400,500%7CMaven+Pro:500">
 
