9a76aba6c53a2149b197d4380d2b018955723609
Rheyan <142854437+RheyanJohn15@users.noreply.github.com>
2024-06-28 12:34:47 +0800

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Models\CshPipeline;
 use App\Jobs\SendEmailJob;
+use App\Models\CshUser;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,6 +26,6 @@ return Application::configure(basePath: dirname(__DIR__))
             foreach($pipeline as $pl){
                 SendEmailJob::dispatch($pl->pl_id);
             }
-        })->timezone('Asia/Hong_Kong')->daily()->at('21:00');
+        })->timezone('Asia/Hong_Kong')->daily()->at('21:00')->emailOutputTo('rheyan@coresupporthub.com');
     })->create();
 
