0b85799dc2970d116e4e7f1d721cced35e917f70
JpUbas <162091712+JpUbas@users.noreply.github.com>
2024-07-03 17:36:38 +0800

    update

--- resources/views/Brochures/index.blade.php
+++ resources/views/Brochures/index.blade.php
@@ -4,6 +4,57 @@
 
     @include('Components.header', ['title'=>'Brochures - Core Support Hub'])
     <style>
+         .modal.fade .modal-dialog {
+            transition: transform 0.3s ease-out, opacity 0.3s ease-out;
+        }
+        .modal.fade.show .modal-dialog {
+            transform: translate(0, 0);
+            opacity: 1;
+        }
+        .modal-dialog {
+            transform: translate(0, -50px);
+            opacity: 0;
+        }
+
+
+#hov {
+    position: relative;
+    display: inline-block;
+}
+
+#hov .overlay-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    background-color: rgba(0, 0, 0, 0.7);
+    padding: 10px 20px;
+    border-radius: 5px;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+    z-index: 2;
+}
+
+#hov::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0);
+    transition: background-color 0.3s ease;
+    z-index: 1;
+}
+
+#hov:hover::after {
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+#hov:hover .overlay-text {
+    opacity: 1;
+}
  .card {
    border: 1px solid #5057624b;
   
@@ -12,11 +63,14 @@
   .card:hover {
     border-color: #c7ae6a; /* Gold border color on hover */
   }
-  @media (min-width: 768px) and (max-width: 991.98px) {
-      .col-md-6 {
-        margin: 20px 0; /* Apply margin for md screen sizes */
-      }
+ @media (min-width: 1000px) and (max-width: 2000px) {
+    .m-3 {
+      width: 18rem !important;
     }
+    .x {
+      width: 130% !important;
+    }
+  }
   </style>
   </head>
   <body >
@@ -35,25 +89,28 @@
       </div>
  
     {{-- main content --}}
-    
 <div class="container col-12" >
   <div class="row">
-  <div class="col-lg-6 col-sm-12">
-<div class="row">
-
-  <div class="card m-3" style="width: 22rem; height:500px;">
-       <div style="width:120%;">
-        <img   src="{{ asset('images/ITmanaged.png') }}" alt="Card image cap">
-    </div>
-      <div class="card-body ">
-        <h5 class="card-title " style="background-color: #22252a;padding-top:3%;padding-bottom:3%;color:white">IT Managed Services</h5>
-        <p class="card-text"> Core Support Hub offers managed IT services including remote desktop support, system administration, and network monitoring to optimize your connectivity. </p>
-       <a href="{{ route('brochures2') }}?Service=ItManageServices" class="btn btn-primary" style="background-color: white; color:#c7ae6a">Learn More ➜</a>
+ 
+   <div class="card m-3" style="width: 22rem; height: 500px;">
+      <div style="width: 120%;" class="x">
+        <img src="{{ asset('images/ITmanaged.png') }}" alt="Card image cap">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title" style="background-color: #22252a; padding-top: 3%; padding-bottom: 3%; color: white;">
+          IT Managed Services
+        </h5>
+        <p class="card-text">
+          Core Support Hub offers managed IT services including remote desktop support, system administration, and network monitoring to optimize your connectivity.
+        </p>
+        <a href="{{ route('brochures2') }}?Service=ItManageServices" class="btn btn-primary" style="background-color: white; color: #c7ae6a;">
+          Learn More ➜
+        </a>
       </div>
     </div>
 
    <div class="card m-3" style="width: 22rem; height:500px;">
-       <div style="width:120%;">
+       <div style="width:120%;" class="x">
         <img   src="{{asset('images/softdev.png')}}" alt="Card image cap">
     </div>
       <div class="card-body ">
@@ -63,14 +120,8 @@
       </div>
     </div>
 
-    </div>
-  </div>
-  
-<div class="col-lg-6 col-sm-12">
-  <div class="row">
-
-        <div class="card m-3" style="width: 22rem; height:500px;">
-       <div style="width:120%;">
+     <div class="card m-3" style="width: 22rem; height:500px;">
+       <div style="width:120%;" class="x">
         <img   src="{{asset('images/bpo.png')}}" alt="Card image cap">
     </div>
       <div class="card-body ">
@@ -81,7 +132,7 @@
     </div>
 
       <div class="card m-3" style="width: 22rem; height:500px;">
-       <div style="width:120%;">
+       <div style="width:120%;" class="x">
         <img   src="{{asset('images/consulting.png')}}" alt="Card image cap">
     </div>
       <div class="card-body ">
@@ -92,8 +143,6 @@
     </div>
 
   </div>
-</div>
- </div>
 </div>
 
 <br>
