dabb1e7d6d43156c0a0c2ec47dd16d1dd3662506
Rheyan <142854437+RheyanJohn15@users.noreply.github.com>
2024-07-08 15:17:36 +0800

    Expand Modal for viewing message

--- resources/views/admin/emailMonitoring.blade.php
+++ resources/views/admin/emailMonitoring.blade.php
@@ -102,7 +102,7 @@
     <!-- Page wrapper end -->
     <div class="modal fade" id="viewMessage" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="staticBackdropLabel">
