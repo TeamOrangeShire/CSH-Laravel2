a4d5d30c5532ccee38d5c4a8038a6e0cc78851d7
Rheyan <142854437+RheyanJohn15@users.noreply.github.com>
2024-07-11 12:18:57 +0800

    try

--- app/Http/Controllers/AdminBackEnd.php
+++ app/Http/Controllers/AdminBackEnd.php
@@ -26,12 +26,44 @@ class AdminBackEnd extends Controller
 {
     public function ScanAttendance(Request $req){
         $user = $req->user_id;
-        $check = CshAttendance::where('user_id', $user)->where('att_status', 0)->first();
-        $config = config('business_time');
+        $check = CshAttendance::where('user_id', $user)->where('att_status', 0)->first(); 
         BusinessTime::enable(Carbon::class, [
+            'monday'    => ['09:00-18:00'],
+            'tuesday'   => ['09:00-18:00'],
+            'wednesday' => ['09:00-18:00'],
+            'thursday'  => ['09:00-18:00'],
+            'friday'    => ['09:00-18:00'],
+            'saturday'  => [],
+            'sunday'    => [],
             'holidaysAreClosed' => true,
-            'holidays' => $config['holidays'],
-            'weekendDays' => $config['weekends'],
+            'holidays'  => [
+                '01-01', // New Year's Day
+                '01-20', // Feast of San Sebastian (Local Holiday)
+                '02-10', // Chinese New Year
+                '02-25', // EDSA People Power Revolution Anniversary
+                '03-28', // Maundy Thursday
+                '03-29', // Good Friday
+                '03-30', // Black Saturday
+                '04-09', // Araw ng Kagitingan
+                '05-01', // Labor Day
+                '06-12', // Independence Day
+                '06-18', // Bacolod City Charter Day (Local Holiday)
+                '08-21', // Ninoy Aquino Day
+                '08-26', // National Heroes Day
+                '11-01', // All Saints' Day
+                '11-02', // All Souls' Day
+                '11-05', // Cinco de Noviembre (Local Holiday)
+                '11-30', // Bonifacio Day
+                '12-08', // Feast of the Immaculate Conception
+                '12-24', // Christmas Eve
+                '12-25', // Christmas Day
+                '12-30', // Rizal Day
+                '12-31', // New Year's Eve
+                '10-18', // Start of MassKara Festival (Local Holiday)
+                '10-19', // Highlight of MassKara Festival (Local Holiday)
+                '10-20', // End of MassKara Festival (Local Holiday)
+                '07-11', //Test Holiday
+            ],
         ]);
         $date = Carbon::now()->setTimezone('Asia/Hong_Kong');
         if($req->code === 'OiFT2qnVVpEn0tmmrkANKHJO6cSOmvQk'){
